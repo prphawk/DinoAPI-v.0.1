33a342df8801aea9bdf4d5ac6e6189c213b43f6f
Joao Pedro Silveira <jpedross1999@gmail.com>
2020-09-04 10:21:00 -0300

    Fix(AppOrigin): Config app origin

--- src/main/java/br/ufrgs/inf/pet/dinoapi/config/AppOriginConfig.java
+++ src/main/java/br/ufrgs/inf/pet/dinoapi/config/AppOriginConfig.java
@@ -1,8 +1,30 @@
 package br.ufrgs.inf.pet.dinoapi.config;
 
+import br.ufrgs.inf.pet.dinoapi.model.app_origin.AppOriginModel;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class AppOriginConfig {
     public String getOrigin() {
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        return "http://localhost:3000";
+        try {
+            final String configFile = new String(
+                    Files.readAllBytes(
+                            Paths.get(getClass().getClassLoader().getResource("app_origin.json").getPath())
+                    )
+            );
+
+            final AppOriginModel appOrigin = objectMapper.readValue(configFile, AppOriginModel.class);
+            return appOrigin.getOrigin();
+        } catch (JsonProcessingException e) {
+            return "";
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
